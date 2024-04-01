b0c4cbd4f860daacfa8be5234b42c6b7e57879fd
Mayur <mayur.fegde@solytics-partners.com>
2024-04-01 20:14:37 +0530

    Updated Init

--- DjKeyCloak/__init__.py
+++ DjKeyCloak/__init__.py
@@ -1,1 +1,13 @@
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+
 from .keycloak_rester import KeycloakRester
+
+missing = []
+for att in ['ssssssssssssssssssssssss', 'REALM_NAME', 'CLIENT']:
+    try:
+        getattr(settings, att)
+    except AttributeError:
+        missing.append(att)
+if missing:
+    raise ImproperlyConfigured(missing)
