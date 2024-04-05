dc886c0f9c68e250aedc05d38b3ad0fca5f34df9
Mayur <mayur.fegde@solytics-partners.com>
2024-04-05 11:51:40 +0530

    Update rester

--- python_keycloak/__init__.py
+++ python_keycloak/__init__.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 
 from .keycloak import KeycloakRester
+from .compose import create_django_user
 
 missing = []
 for att in ['KEYCLOAK_SERVER', 'REALM_NAME', 'CLIENT']:
